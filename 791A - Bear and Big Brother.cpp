862d4b64e6fbb30c752da14d11ef96797b4615e4
Mujahid Ansori Majid <63152578+hitz304@users.noreply.github.com>
2020-05-10 20:23:50 +0700

    Create 791A - Bear and Big Brother.cpp

---
+++ 791A - Bear and Big Brother.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main(){
+    int a,b;
+    int count=0;
+    cin >> a >> b;
+    while(a <= b){
+    	a *= 3;
+    	b *= 2;
+    	count++;
+	}
+	cout << count;
+    return 0;
+}
