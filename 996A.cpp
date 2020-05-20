966d01f3b90c11e5d7715e51101ad96c8039f600
Mujahid Ansori Majid <63152578+hitz304@users.noreply.github.com>
2020-05-20 14:10:09 +0700

    Create 996A.cpp

---
+++ 996A.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int n, cnt = 0;
+	cin >> n;
+	while(n > 0){
+		if(n - 100 >= 0){
+			n -= 100;
+			cnt++;
+		}
+		else if(n - 20 >= 0){
+			n -= 20;
+			cnt++;
+		}
+		else if(n - 10 >= 0){
+			n -= 10;
+			cnt++;
+		}
+		else if(n - 5 >= 0){
+			n -= 5;
+			cnt++; 
+		}
+		else {
+			n--;
+			cnt++;
+		}
+	}
+	cout << cnt << endl;
+	return 0;
+}
